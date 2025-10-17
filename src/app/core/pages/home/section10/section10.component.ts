516f6d74e1677b28659d0d26a0b4845bacb9b3e0
salahyahya1 <salahyahyaed@gmail.com>
2025-10-17 22:24:46 +0300

    last version to test

--- src/app/core/pages/home/section10/section10.component.ts
+++ src/app/core/pages/home/section10/section10.component.ts
@@ -33,7 +33,7 @@ export class Section10Component {
               trigger: '#section100',
               start: 'top top',
               end: "100% bottom",
-              markers: true,
+              // markers: true,
               // pin: true,
               anticipatePin: 1,
             },
