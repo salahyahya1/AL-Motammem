f8f382d359d15c204016e3dd205e342bc30ae424
salahyahya1 <salahyahyaed@gmail.com>
2025-12-14 04:22:48 +0200

    some fixes

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink,TranslatePipe],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  @Input() phone = '201016889890';
 
+  // الرسالة الجاهزة
+  @Input() message = 'مرحبًا، أريد الاستفسار عن خدماتكم.';
+
+  get link(): string {
+    return `https://wa.me/${this.phone}?text=${encodeURIComponent(this.message)}`;
+  }
+
+  open() {
+    window.open(this.link, '_blank', 'noopener,noreferrer');
+  }
 }
